07ea506cf0d7be2ab1d051c754a23e3395311315
Jaren Kempton <48076699+JarenKempton@users.noreply.github.com>
2026-02-10 17:39:07 -0700

    test

--- packages/types/src/enums/BillingPeriod.ts
+++ packages/types/src/enums/BillingPeriod.ts
@@ -2,5 +2,6 @@ export const BillingPeriod = {
   MONTHLY: 'MONTHLY',
   YEARLY: 'YEARLY',
   LIFETIME: 'LIFETIME',
+  TEST: "Testing"
 } as const;
 export type BillingPeriod = (typeof BillingPeriod)[keyof typeof BillingPeriod];
